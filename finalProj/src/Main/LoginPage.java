ace0e705e138e05bcfd7ee9b54b53436c8e0e9e4
Sean <Sean@SeanMathew>
2023-08-27 13:03:01 +0800

    Restored the functionality of the buttons in LoginPage as it was removed

--- finalProj/src/Main/LoginPage.java
+++ finalProj/src/Main/LoginPage.java
@@ -21,7 +21,7 @@ import Main.RegisterPage;
 
 import utils.WriteReadHandler;
 
-public class LoginPage implements KeyListener{
+public class LoginPage implements KeyListener {
 	private JPanel contentPane;
 	private JLabel messageLabel = new JLabel();
 	private JFrame frame = new JFrame();
@@ -30,7 +30,7 @@ public class LoginPage implements KeyListener{
 	private JPasswordField passwordField;
 
 	LoginPage() throws IOException {
-		
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100, 100, 471, 399);
 		contentPane = new JPanel();
@@ -38,54 +38,83 @@ public class LoginPage implements KeyListener{
 
 		frame.setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel header = new JPanel();
 		header.setBounds(0, 0, 453, 61);
 		header.setBorder(new EmptyBorder(0, 30, 0, 0));
 		header.setBackground(new Color(43, 52, 59));
 		contentPane.add(header);
 		header.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Welcome!");
 		lblNewLabel.setBounds(35, 15, 129, 32);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 26));
 		lblNewLabel.setForeground(new Color(255, 255, 255));
 		header.add(lblNewLabel);
-		
+
 		JLabel lblNewLabel_1_1 = new JLabel("Username: ");
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_1.setBounds(74, 102, 128, 25);
 		contentPane.add(lblNewLabel_1_1);
-		
+
 		usernameField = new JTextField();
 		usernameField.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		usernameField.setColumns(10);
 		usernameField.setBounds(74, 126, 293, 25);
 		contentPane.add(usernameField);
-		
+
 		JLabel lblNewLabel_1_1_1 = new JLabel("Password: ");
 		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1_1_1.setBounds(74, 166, 102, 23);
 		contentPane.add(lblNewLabel_1_1_1);
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setBounds(74, 189, 293, 25);
 		contentPane.add(passwordField);
-		
+
 		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					loginAccount();
+				} catch (HeadlessException | ClassNotFoundException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		btnLogin.setFont(new Font("Tahoma", Font.ITALIC, 13));
 		btnLogin.setBackground(new Color(201, 242, 168));
 		btnLogin.setBounds(74, 251, 142, 35);
 		contentPane.add(btnLogin);
-		
+
 		JButton btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					RegisterPage regPage = new RegisterPage(idAndPass.getLoginInfo());
+				} catch (ClassNotFoundException | IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				frame.dispose();
+			}
+		});
 		btnRegister.setFont(new Font("Tahoma", Font.ITALIC, 13));
 		btnRegister.setBackground(new Color(187, 214, 249));
 		btnRegister.setBounds(225, 251, 142, 35);
 		contentPane.add(btnRegister);
-		
+
 		JButton adminLoginBtn = new JButton("Log-in as Admin");
+		adminLoginBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.setEnabled(false);
+				AdminLogin adminLogin = new AdminLogin(idAndPass.getAdminLoginInfo(), frame);
+			}
+		});
 		adminLoginBtn.setBackground(new Color(226, 252, 252));
 		adminLoginBtn.setBounds(158, 298, 128, 25);
 		contentPane.add(adminLoginBtn);
@@ -118,23 +147,24 @@ public class LoginPage implements KeyListener{
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	private void loginAccount() throws HeadlessException, FileNotFoundException, ClassNotFoundException, IOException {
 		String userID = usernameField.getText();
 		String password = String.valueOf(passwordField.getPassword());
 
 		if (idAndPass.getLoginInfo().containsKey(userID)) {
 			if (idAndPass.getLoginInfo().get(userID).equals(password)) {
-				
-				frame.dispose();;
-				Dashboard window = new Dashboard();			
-				
+
+				frame.dispose();
+				;
+				Dashboard window = new Dashboard();
+
 			} else {
-				JOptionPane.showMessageDialog(null,"Wrong password or username");
+				JOptionPane.showMessageDialog(null, "Wrong password or username");
 			}
 
 		} else {
-			JOptionPane.showMessageDialog(null,"Wrong password or username");
+			JOptionPane.showMessageDialog(null, "Wrong password or username");
 		}
 	}
 }
