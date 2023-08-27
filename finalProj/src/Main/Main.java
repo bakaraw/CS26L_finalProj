7956da52057125595a9ae278cd81c0051c2c9a5a
Abcd <Abcd@DESKTOP-JK81HE2>
2023-08-27 14:50:40 +0800

    sysout "wo let the dogs out"

--- finalProj/src/Main/Main.java
+++ finalProj/src/Main/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		LoginPage loginPage = new LoginPage();
+		System.out.println("Wo let the dogs out");
 	}
 
 }
